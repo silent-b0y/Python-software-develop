c6790d6623ff80546e09d8c11b1eda6dadbffc97
Денис Набережнов <115325817+silent-b0y@users.noreply.github.com>
2023-10-26 14:18:43 +0300

    Update task_2.py

--- lab_1/task_2.py
+++ lab_1/task_2.py
@@ -1,5 +1,11 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-count_of_symbols = 25 * 50 * 100
-one_book = count_of_symbols * 4
-mb = one_book / 1024 / 1024
-print("Количество книг, помещающихся на дискету:", int(1.44 // mb))
+ONE_CHAR_BYTES = 4
+
+count_of_pages = 100
+count_of_strings = 50
+count_of_chars = 25
+
+count_of_symbols = count_of_chars * count_of_strings * count_of_pages
+one_book_size_bytes = count_of_symbols * ONE_CHAR_BYTES
+one_book_size_mbytes = one_book_size_bytes / 1024 / 1024
+print("Количество книг, помещающихся на дискету:", int(1.44 // one_book_size_mbytes))
